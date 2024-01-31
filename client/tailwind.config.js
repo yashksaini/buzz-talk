6a63a8cd7d8e116330170206621581c7a6c090c2
yashksaini <yashdms1@gmail.com>
2024-01-31 10:07:13 +0530

    feat: :art: Login and Signup Page UI updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js,jsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: "#F69C14",
+      secondary: "#1B9A41",
+      primaryHover: "#ff9900",
+      background: "#fff",
+      dark1: "#303030",
+      dark2: "#1f1f1f",
+    },
+  },
 };
 export const plugins = [];
