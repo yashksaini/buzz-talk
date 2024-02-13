f638256a1ed787c080e488cc775734df14924ad9
yashksaini <yashdms1@gmail.com>
2024-02-13 09:20:06 +0530

    style: :art: UI changed for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,47 @@
 import axios from "axios";
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { BASE_URL } from "../main";
 import logo from "../assets/logo1.png";
-import { RxDashboard } from "react-icons/rx";
-import { CiUser } from "react-icons/ci";
-import { LuMessagesSquare } from "react-icons/lu";
-import { LiaUserFriendsSolid } from "react-icons/lia";
-import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdOutlineDashboard, MdDashboard } from "react-icons/md";
+import { FaRegUser, FaUser } from "react-icons/fa6";
+import { IoChatbubblesOutline, IoChatbubbles } from "react-icons/io5";
+import { HiOutlineUsers, HiMiniUsers } from "react-icons/hi2";
+import { BsBell, BsBellFill } from "react-icons/bs";
 const AdminNav = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { fullName, userId } = useSelector((state) => state.userAuth);
   const adminNavs = [
-    { text: "Dashboard", link: "/", icon: <RxDashboard /> },
-    { text: "Profile", link: `/profile/${userId}`, icon: <CiUser /> },
-    { text: "Messages", link: "/chats", icon: <LuMessagesSquare /> },
-    { text: "Friends", link: "/friends", icon: <LiaUserFriendsSolid /> },
+    {
+      text: "Dashboard",
+      link: "/",
+      icon: <MdOutlineDashboard />,
+      activeIcon: <MdDashboard />,
+    },
+    {
+      text: "Profile",
+      link: `/profile/${userId}`,
+      icon: <FaRegUser />,
+      activeIcon: <FaUser />,
+    },
+    {
+      text: "Messages",
+      link: "/chats",
+      icon: <IoChatbubblesOutline />,
+      activeIcon: <IoChatbubbles />,
+    },
+    {
+      text: "Friends",
+      link: "/friends",
+      icon: <HiOutlineUsers />,
+      activeIcon: <HiMiniUsers />,
+    },
     {
       text: "Notifications",
       link: "/notifications",
-      icon: <IoIosNotificationsOutline />,
+      icon: <BsBell />,
+      activeIcon: <BsBellFill />,
     },
   ];
 
@@ -30,12 +51,14 @@ const AdminNav = () => {
   };
 
   return (
-    <nav className="bg-gray-100 h-full p-2">
-      <div className="w-full flex flex-1 gap-1 justify-start items-center">
-        <img src={logo} alt="Logo" className="h-10" />
-        <span className=" text-primary text-xl font-bold leading-10">TALK</span>
+    <nav className=" h-full p-2">
+      <div className="w-full flex flex-1 gap-1 justify-start items-center px-3 py-4">
+        <img src={logo} alt="Logo" className="h-12" />
+        <span className=" text-primary text-2xl font-bold leading-10">
+          TALK
+        </span>
       </div>
-      <div className="mt-8">
+      <div className="mt-4">
         {adminNavs.map((nav, index) => {
           return (
             <NavLink
@@ -43,11 +66,12 @@ const AdminNav = () => {
               key={index}
               className={({ isActive }) =>
                 isActive
-                  ? "text-white text-md flex justify-start items-center gap-2 text-xl bg-dark2 h-12 mb-4 rounded-md border"
-                  : "text-gray-700 text-md flex justify-start items-center gap-2 text-xl  h-12 bg-white  mb-4 rounded-md border"
+                  ? "group text-dark1 font-semibold text-md flex justify-start items-center gap-2 text-xl h-12 mb-4 activeNav"
+                  : "group text-mainText text-md flex justify-start items-center gap-2 text-xl  h-12 mb-4 defaultNav"
               }
             >
-              <div className="inline-flex">
+              <div className="inline-flex group-hover:bg-backgroundDark transition duration-300 ease-in-out justify-center items-center gap-3 px-5 h-12 rounded-full">
+                <span>{nav.activeIcon}</span>
                 <span>{nav.icon}</span>
                 <span>{nav.text}</span>
               </div>
