b07747db5cd4bfd77cc8b08f53e6e763ad4630fd
yashksaini <yashdms1@gmail.com>
2024-03-01 22:02:30 +0530

    text updated

--- client/src/user/Friends.jsx
+++ client/src/user/Friends.jsx
@@ -7,7 +7,7 @@ const Friends = () => {
         <div className="w-full px-4  sticky top-0 bg-white h-16 flex justify-center items-start flex-col z-10 border-b">
           <h1 className="text-2xl font-bold text-dark1 leading-7">Friends</h1>
           <p className="text-mainText leading-4 flex justify-start items-center gap-1 text-sm  overflow-hidden text-ellipsis whitespace-nowrap">
-            All your friends list
+            All your friends list appear below
           </p>
         </div>
       </div>
