c8554fc1b020450e4feebae3db4958eab5c1a006
yashksaini <yashdms1@gmail.com>
2024-10-20 17:09:40 +0530

    feat: :sparkles: Friends List on the Messages Page

---
+++ client/src/components/UI/FriendsChatCard.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import ProfileIcon from "../ProfileIcon";
+import PropTypes from "prop-types";
+const FriendsChatCard = ({ user }) => {
+  return (
+    <div
+      key={user?.username}
+      className={`w-full py-3 px-3 flex justify-start items-start gap-2 hover:bg-line cursor-pointer`}
+    >
+      <div className="min-w-10 min-h-10 rounded-full flex justify-center items-center border border-primaryBorder bg-transPrimary">
+        {!user?.imgUrl && <ProfileIcon fullName={user?.fullName} />}
+        {user?.imgUrl && (
+          <Link to={"/profile/" + user?.username}>
+            <img
+              src={user?.imgUrl}
+              alt="profile"
+              className="w-9 h-9 rounded-full"
+            />
+          </Link>
+        )}
+      </div>
+      <div className="w-[calc(100%_-_52px)] ">
+        <div className=" h-10 flex justify-between items-center  gap-[2px]">
+          <div>
+            <p className="leading-4 whitespace-nowrap overflow-hidden text-ellipsis text-dark1 font-semibold ">
+              {user?.fullName}
+            </p>
+            <p className="leading-4  text-mainText text-xs ">
+              @{user?.username}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+// Prop types validation
+FriendsChatCard.propTypes = {
+  user: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    fullName: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    imgUrl: PropTypes.string,
+  }).isRequired,
+};
+
+export default FriendsChatCard;
--- client/src/user/MyChats.jsx
+++ client/src/user/MyChats.jsx
@@ -1,5 +1,121 @@
+import { useEffect, useState } from "react";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BASE_URL } from "../main";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import Loader from "../components/UI/Loader";
+import NoDataFound from "../components/UI/NoDataFound";
+import FriendsChatCard from "../components/UI/FriendsChatCard";
 const MyChats = () => {
-  return <div>MyChats</div>;
+  const { userId } = useSelector((state) => state.userAuth);
+  const [search, setSearch] = useState("");
+  const [friendsList, setFriendsList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const handleSearch = () => {};
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/friends/getFriendsList`, {
+          params: {
+            ownerId: userId,
+          },
+        });
+        if (response.data) {
+          setFriendsList(response?.data?.friends || []);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log("ERROR in fetching friends", error);
+        setLoading(false);
+      }
+    };
+    fetchFriends();
+  }, [userId]);
+  return (
+    <>
+      <div className="flex justify-center items-center h-full">
+        {/* Friends List */}
+        <div className="flex-1 bg-white h-full overflow-y-auto overflow-x-hidden border-r border-line text-dark1">
+          {/* Top Section */}
+          <div className="px-3">
+            <div className="w-full flex justify-between items-center py-2">
+              <span className="text-2xl font-bold text-dark1 leading-7 min-w-48">
+                Messages
+              </span>
+              <span className="hover:bg-line p-2 rounded-full">
+                <IoSettingsOutline
+                  className="text-xl cursor-pointer"
+                  data-tooltip-id="settings-tip"
+                />
+              </span>
+
+              <ReactTooltip
+                id="settings-tip"
+                place="bottom"
+                content="Settings"
+                style={{
+                  zIndex: "50",
+                }}
+              />
+            </div>
+            <div className="w-full bg-white">
+              <div className="w-full h-12 bg-white rounded-full border border-borderColor flex justify-center items-center gap-3 pl-3 pr-2 focus-within:bg-white focus-within:border-primaryBorder ">
+                <search className="text-2xl group:focus-within text-primary " />
+                <input
+                  type="text"
+                  placeholder="Search Direct Messages..."
+                  className="flex-1 bg-transparent focus:outline-none text-dark1 font-medium"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+                <button
+                  onClick={handleSearch}
+                  className="bg-dark2 text-white h-8 px-4 rounded-full"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Friends List */}
+          <div className="mt-4">
+            {loading && (
+              <div className="w-full flex justify-center items-center min-h-40">
+                <Loader />
+              </div>
+            )}
+            {!loading &&
+              friendsList?.map((friend, index) => {
+                return (
+                  <FriendsChatCard
+                    user={{
+                      _id: friend._id,
+                      fullName: friend.fullName,
+                      username: friend.username,
+                      imgUrl: friend.imgUrl,
+                    }}
+                    key={index}
+                  />
+                );
+              })}
+            {!loading && friendsList.length === 0 && (
+              <NoDataFound
+                title="No Friends"
+                desc="You haven't added any friends yet. Once you connect with others, your friends will appear here."
+              />
+            )}
+          </div>
+        </div>
+        {/* Chat Area */}
+        <div className="w-[600px] border-r border-line h-full overflow-y-auto overflow-x-hidden">
+          Chat Area
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MyChats;
